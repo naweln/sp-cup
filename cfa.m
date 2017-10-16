c97ec7bcd6c1b045a86518fe82375c64f53843eb
wodw0 <nawel.naas@gmail.com>
2017-10-16 19:16:21 +0200

    cfa matrix

---
+++ cfa.m
@@ -0,0 +1,28 @@
+clear all
+path = '/Users/Nawel/Desktop/SPCup18/data/Sony-NEX-7/DSC01079.JPG';
+image = imread(path);
+imsize = size(image);
+
+%% DOWNSAMPLING
+image_down = image(1:20:end, 1:20:end, :);
+
+%% BAYER CFA 
+% red = 1, blue = 2, green = 3
+
+p(1,1) = 2; p(1,2) = 3; p(2,1) = 3; p(2,2) = 1; 
+
+P = zeros(size(p,1), size(p,2), 3);
+for m=1:3
+    i = find(p == m); 
+    [j,k] = ind2sub(size(p),i);
+    for l=1:length(j)
+        P(j(l),k(l),m) = 1; 
+    end 
+end
+
+r = imsize(1)/length(p);
+c = imsize(2)/length(p);
+CFA = repmat(P,r,c,1);
+
+
+
